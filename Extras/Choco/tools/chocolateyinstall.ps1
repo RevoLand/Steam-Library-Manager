f79ab3d0214fc767d398a5380d048bbd6ce54ae0
Mert Ercan <revoland@windowslive.com>
2019-11-21 21:32:23 +0300

    Choco files

---
+++ Extras/Choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$toolsPath  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+$packageArgs = @{
+    PackageName  = "steamlibrarymanager"
+    File         = "$toolsPath\Steam.Library.Manager.zip"
+    Destination  = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
+ 
+Remove-Item -force "$toolsPath\*.zip" -ea 0
