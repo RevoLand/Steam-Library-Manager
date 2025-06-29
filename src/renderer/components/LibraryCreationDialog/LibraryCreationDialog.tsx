c3fb6e66f5a01f3f1934a3efaa5f8c3850e340b4
Mert Ercan <revoland@windowslive.com>
2025-06-29 19:42:42 +0300

    add react-hook-form for form management

--- src/renderer/components/LibraryCreationDialog/LibraryCreationDialog.tsx
+++ src/renderer/components/LibraryCreationDialog/LibraryCreationDialog.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import LibraryType from 'src/core/models/LibraryType';
 import useLibraries from 'src/renderer/hooks/useLibraries';
 import { Button } from '../ui/button';
@@ -7,34 +8,37 @@ import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
 
+type LibraryCreationForm = {
+  path: string;
+  label: string;
+  platform: LibraryType;
+};
+
 const LibraryCreationDialog = () => {
-  const [selectedPath, setSelectedPath] = useState('');
   const [showLabelDialog, setShowLabelDialog] = useState(false);
-  const [label, setLabel] = useState('');
-  const [platform, setPlatform] = useState<LibraryType>('slm');
   const { create, refreshLibraries } = useLibraries();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+    setValue,
+    reset,
+  } = useForm<LibraryCreationForm>({
+    defaultValues: {
+      platform: 'slm',
+    },
+  });
 
-  const openLibraryDirectorySelector = async () => {
-    const path = await window.api['select-directory']();
-
-    if (!path) {
-      return;
-    }
-
-    setSelectedPath(path);
-    setShowLabelDialog(true);
-  };
-
-  const resetDialog = () => {
-    setSelectedPath('');
-    setLabel('');
+  const resetDialog = useCallback(() => {
     setShowLabelDialog(false);
-    setPlatform('slm');
-  };
+    reset();
+  }, []);
 
-  const createLibrary = async () => {
+  const onSubmit: SubmitHandler<LibraryCreationForm> = async (libraryData) => {
     try {
-      await create(selectedPath, label, platform);
+      await create(libraryData.path, libraryData.label, libraryData.platform);
       refreshLibraries();
 
       resetDialog();
@@ -43,49 +47,63 @@ const LibraryCreationDialog = () => {
     }
   };
 
+  const openLibraryDirectorySelector = async () => {
+    const path = await window.api['select-directory']();
+
+    if (!path) {
+      return;
+    }
+
+    setValue('path', path);
+    setShowLabelDialog(true);
+  };
+
   return (
     <>
       <button className='text-blue-600 hover:underline text-left' onClick={() => openLibraryDirectorySelector()}>
         + Kütüphane Oluştur
       </button>
       {showLabelDialog && (
-        <Dialog defaultOpen onOpenChange={resetDialog}>
+        <Dialog open onOpenChange={resetDialog}>
           <DialogContent className='bg-white'>
             <DialogHeader>
               <DialogTitle>Yeni Kütüphane Oluştur</DialogTitle>
             </DialogHeader>
-            <div className='mb-2 select-none'>
-              📁 {selectedPath} <span onClick={() => openLibraryDirectorySelector()}>🔃</span>
-            </div>
-            <label htmlFor='label' className='text-sm font-medium'>
-              Etiket (İsteğe Bağlı)
-            </label>
-            <Input
-              id='label'
-              placeholder='Örn: SSD Oyunlar, Arşiv, Retro...'
-              value={label}
-              onChange={(e) => setLabel(e.target.value)}
-              autoFocus
-            />
-            <label htmlFor='label' className='text-sm font-medium'>
-              Platform
-            </label>
-            <RadioGroup value={platform} onValueChange={(value) => setPlatform(value as LibraryType)}>
-              <div className='flex items-center space-x-2'>
-                <RadioGroupItem value='steam' id='steam' />
-                <Label htmlFor='steam'>Steam</Label>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className='mb-2 select-none'>
+                📁 {watch('path')} <span onClick={() => openLibraryDirectorySelector()}>🔃</span>
               </div>
-              <div className='flex items-center space-x-2'>
-                <RadioGroupItem value='slm' id='slm' />
-                <Label htmlFor='slm'>SLM</Label>
+              <label htmlFor='label' className='text-sm font-medium'>
+                Etiket (İsteğe Bağlı)
+              </label>
+              <Input id='label' placeholder='Örn: SSD Oyunlar, Arşiv, Retro...' {...register('label')} autoFocus />
+              <label htmlFor='label' className='text-sm font-medium'>
+                Platform
+              </label>
+              <Controller
+                name='platform'
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <RadioGroup {...field} onValueChange={field.onChange} value={field.value}>
+                    <div className='flex items-center space-x-2'>
+                      <RadioGroupItem value='steam' id='steam' />
+                      <Label htmlFor='steam'>Steam</Label>
+                    </div>
+                    <div className='flex items-center space-x-2'>
+                      <RadioGroupItem value='slm' id='slm' />
+                      <Label htmlFor='slm'>SLM</Label>
+                    </div>
+                  </RadioGroup>
+                )}
+              />
+              <div className='mt-4 flex justify-end gap-2'>
+                <Button variant='ghost' onClick={() => resetDialog()}>
+                  Vazgeç
+                </Button>
+                <Button type='submit'>Ekle</Button>
               </div>
-            </RadioGroup>
-            <div className='mt-4 flex justify-end gap-2'>
-              <Button variant='ghost' onClick={() => resetDialog()}>
-                Vazgeç
-              </Button>
-              <Button onClick={createLibrary}>Ekle</Button>
-            </div>
+            </form>
           </DialogContent>
         </Dialog>
       )}
